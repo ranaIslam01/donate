2075cb353f92ed4d24e39c0381d49777169bcb85
Rana Islam <dev.rana.cse@gmail.com>
2025-10-29 16:19:16 +0600

    Scroll.js

--- js/script.js
+++ js/script.js
@@ -72,31 +72,4 @@ buttons.forEach((btn) => {
 // Section End 
 
 
-//Navbar start
-let lastScroll = 0;
-const navbar = document.getElementById("navbar");
-
-window.addEventListener("scroll", () => {
-  const currentScroll = window.scrollY;
-
-  if (currentScroll > lastScroll && currentScroll > 100) {
-    // স্ক্রল ডাউন হলে navbar hide হবে
-    navbar.style.transform = "translateY(-100%)";
-    navbar.style.transition = "transform 0.5s ease";
-    navbar.style.backgroundColor = "white"; // background white
-    navbar.style.boxShadow = "0 4px 6px rgba(0,0,0,0.1)"; // soft shadow
-    navbar.style.backdropFilter = "none"; // transparency remove
-  } else if (currentScroll < lastScroll) {
-    // স্ক্রল আপ হলে navbar show হবে
-    navbar.style.transform = "translateY(0)";
-    navbar.style.transition = "transform 0.5s ease";
-    navbar.style.backgroundColor = "white";
-    navbar.style.boxShadow = "0 4px 6px rgba(0,0,0,0.1)";
-    navbar.style.backdropFilter = "none";
-  }
-
-  lastScroll = currentScroll;
-});
-
-//Navbar end
 
